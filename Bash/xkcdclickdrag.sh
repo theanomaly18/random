1dd43f8a60404ab2900d7412dd66f926b3e7a625
Phil Shaw <philshaw13@yahoo.com>
2012-09-24 11:42:43 -0600

    Fixed issue. Had to reset num2 after it finishes inner loop to get it to work.

--- Bash/xkcdclickdrag.sh
+++ Bash/xkcdclickdrag.sh
@@ -10,53 +10,57 @@ lettere=e
 site="http://imgs.xkcd.com/clickdrag/"
 format=".png"
 
-while [ $num1 -lt 20 ]; do
+while [ $num1 -lt 50 ]; do
   let num1=$num1
-  while [ $num2 -lt 20 ]; do
-    let num2=$num2
+  while [ $num2 -lt 50 ]; do
+    #let num2=$num2
 	wget -N ${site}${num1}${lettern}${num2}${letterw}${format}
 	let num2+=1
   done
   let num1+=1
+  let num2=1
 done
 
 num1=1
 num2=1
 
-while [ $num1 -lt 20 ]; do
+while [ $num1 -lt 50 ]; do
   let num1=$num1
-  while [ $num2 -lt 20 ]; do
+  while [ $num2 -lt 50 ]; do
     let num2=$num2
 	wget -N ${site}${num1}${lettern}${num2}${lettere}${format}
 	let num2+=1
   done
   let num1+=1
+  let num2=1
 done
 
 num1=1
 num2=1
 
-while [ $num1 -lt 20 ]; do
+while [ $num1 -lt 50 ]; do
   let num1=$num1
-  while [ $num2 -lt 20 ]; do
+  while [ $num2 -lt 50 ]; do
     let num2=$num2
 	wget -N ${site}${num1}${letters}${num2}${lettere}${format}
 	let num2+=1
   done
   let num1+=1
+  let num2=1
 done
  
 num1=1
 num2=1
 
-while [ $num1 -lt 20 ]; do
+while [ $num1 -lt 50 ]; do
   let num1=$num1
-  while [ $num2 -lt 20 ]; do
+  while [ $num2 -lt 50 ]; do
     let num2=$num2
 	wget -N ${site}${num1}${letters}${num2}${letterw}${format}
 	let num2+=1
   done
   let num1+=1
+  let num2=1
 done
 
 #EOF
