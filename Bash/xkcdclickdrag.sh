c141f93f05872c29f3c133fc8224ee5532381bff
Phil Shaw <philshaw13@yahoo.com>
2012-09-24 11:10:22 -0600

    Added new bash script for downloading the xkcd images for click and drag comic. Still needs work, num1 does not update, so not getting all images.

---
+++ Bash/xkcdclickdrag.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+num1=1
+num2=1
+lettern=n
+letters=s
+letterw=w
+lettere=e
+
+site="http://imgs.xkcd.com/clickdrag/"
+format=".png"
+
+while [ $num1 -lt 20 ]; do
+  let num1=$num1
+  while [ $num2 -lt 20 ]; do
+    let num2=$num2
+	wget -N ${site}${num1}${lettern}${num2}${letterw}${format}
+	let num2+=1
+  done
+  let num1+=1
+done
+
+num1=1
+num2=1
+
+while [ $num1 -lt 20 ]; do
+  let num1=$num1
+  while [ $num2 -lt 20 ]; do
+    let num2=$num2
+	wget -N ${site}${num1}${lettern}${num2}${lettere}${format}
+	let num2+=1
+  done
+  let num1+=1
+done
+
+num1=1
+num2=1
+
+while [ $num1 -lt 20 ]; do
+  let num1=$num1
+  while [ $num2 -lt 20 ]; do
+    let num2=$num2
+	wget -N ${site}${num1}${letters}${num2}${lettere}${format}
+	let num2+=1
+  done
+  let num1+=1
+done
+ 
+num1=1
+num2=1
+
+while [ $num1 -lt 20 ]; do
+  let num1=$num1
+  while [ $num2 -lt 20 ]; do
+    let num2=$num2
+	wget -N ${site}${num1}${letters}${num2}${letterw}${format}
+	let num2+=1
+  done
+  let num1+=1
+done
+
+#EOF
